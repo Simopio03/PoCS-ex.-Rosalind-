928aad5959b21ac774582a025ab9c4b808b1d1fa
Simopio03 <149507459+Simopio03@users.noreply.github.com>
2023-11-17 17:34:19 +0100

    Add files via upload

---
+++ 2_ass_ex_5.py
@@ -0,0 +1,55 @@
+#5th ex
+
+
+
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from collections import defaultdict
+
+def find_corrections(fasta_file):
+    sequences = defaultdict(list)
+    corrections = []
+
+    for record in SeqIO.parse(fasta_file, "fasta"):
+        seq = str(record.seq)
+        rev_seq = str(record.seq.reverse_complement())
+
+        if seq in sequences:
+            corrections.append(f"{seq}->{seq}")
+        elif rev_seq in sequences:
+            corrections.append(f"{seq}->{rev_seq}")
+        else:
+            sequences[seq].append(record.id)
+
+    return corrections
+
+fasta_file = "rosalind_corr.txt"
+corrections = find_corrections(fasta_file)
+
+for correction in corrections:
+    print(correction)
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+             
+
+
+
+
+
---
+++ 2_ass_ex_6.py
@@ -0,0 +1,9 @@
+#6th ex
+
+
+def count_internal_nodes(n):
+    return n - 2
+
+n_leaves = 6482
+result = count_internal_nodes(n_leaves)
+print( n_leaves, result)
